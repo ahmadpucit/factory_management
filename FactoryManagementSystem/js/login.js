913f7f8ee79ca3aa3cb7a1be69bfcf34f05b24c4
ahmadpucit <bcsf11m049@pucit.edu.pk>
2015-02-22 04:23:57 +0500

    Update login.js

--- FactoryManagementSystem/js/login.js
+++ FactoryManagementSystem/js/login.js
@@ -1,7 +1,5 @@
 
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
-// Sets the min-height of #page-wrapper to window size
+
 $(function() {
     $('#login_btn').click(function () {
         var login_name = $('#user_name').val();
